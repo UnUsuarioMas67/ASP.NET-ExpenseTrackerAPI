045b14c089574c1a527b2f7cf4b7a98b73a5984c
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-09-15 21:25:17 -0400

    Add SQL stored procedures for adding and retrieving expenses

--- sql/schema.sql
+++ sql/schema.sql
@@ -45,3 +45,40 @@ CREATE TABLE UserExpenses (
 )
 
 SELECT * FROM ExpenseCategories
+GO;
+
+CREATE OR ALTER PROC sp_AddExpense (@Description VARCHAR(200), @Amount DECIMAL, @ExpenseDate DATE, @CategoryId VARCHAR(25), @UserEmail VARCHAR(254))
+AS
+BEGIN
+	INSERT INTO Expenses ([Description], Amount, ExpenseDate, CategoryId)
+	VALUES (@Description, @Amount, @ExpenseDate, @CategoryId)
+
+	DECLARE @ExpenseId INT, @UserId INT
+
+	SET @ExpenseId = SCOPE_IDENTITY()
+	SELECT @UserId = UserId FROM Users
+	WHERE Email = @UserEmail
+
+	INSERT INTO UserExpenses (UserId, ExpenseId)
+	VALUES (@UserId, @ExpenseId)
+
+	SELECT ExpenseId, [Description], Amount, ExpenseDate as [Date], c.CategoryId, c.CategoryName 
+	FROM Expenses e
+	JOIN ExpenseCategories c ON e.CategoryId = c.CategoryId
+	WHERE e.ExpenseId = @ExpenseId
+END
+GO
+
+CREATE OR ALTER PROC sp_GetExpensesFromUser (@UserEmail VARCHAR(254))
+AS
+BEGIN
+	SELECT e.ExpenseId, e.[Description], e.Amount, e.ExpenseDate AS [Date], c.CategoryId, c.CategoryName
+	FROM Expenses e
+	JOIN ExpenseCategories c ON e.CategoryId = c.CategoryId
+	JOIN UserExpenses ue ON e.ExpenseId = ue.ExpenseId
+	JOIN Users u ON ue.UserId = u.UserId
+	WHERE u.Email = @UserEmail
+END
+GO
+
+EXEC sp_GetExpensesFromUser 'admin1234@email.com'
