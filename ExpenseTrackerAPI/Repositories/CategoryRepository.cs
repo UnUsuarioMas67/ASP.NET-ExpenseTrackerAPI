50d39866aae6da319076faac6dec57489b4b19d1
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-09-15 21:38:39 -0400

    Add comment at CategoryRepository.cs

--- ExpenseTrackerAPI/Repositories/CategoryRepository.cs
+++ ExpenseTrackerAPI/Repositories/CategoryRepository.cs
@@ -11,13 +11,24 @@ public interface ICategoryRepository
 
 public class CategoryRepository(IConfiguration configuration) : ICategoryRepository
 {
+    /*
+     * -- VALID CATEGORIES IDs --
+     * clothing
+     * electronics
+     * groceries
+     * health
+     * leisure
+     * other
+     * utilities
+     */
+    
     public async Task<Category?> GetCategoryByIdAsync(string id)
     {
         var sql = "SELECT CategoryId, CategoryName FROM ExpenseCategories Where CategoryId = @id";
-        
+
         await using var conn = new SqlConnection(configuration.GetConnectionString("ExpenseTracker"));
         await conn.OpenAsync();
-        
+
         var category = await conn.QuerySingleOrDefaultAsync<Category>(sql, new { id });
         return category;
     }
@@ -25,10 +36,10 @@ public class CategoryRepository(IConfiguration configuration) : ICategoryReposit
     public async Task<List<Category>> GetAllCategoriesAsync()
     {
         var sql = "SELECT CategoryId, CategoryName FROM ExpenseCategories";
-        
+
         await using var conn = new SqlConnection(configuration.GetConnectionString("ExpenseTracker"));
         await conn.OpenAsync();
-        
+
         var categories = await conn.QueryAsync<Category>(sql);
         return categories.ToList();
     }
