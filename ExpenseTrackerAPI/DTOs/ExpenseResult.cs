eee04bad0d22e07cefdcda7d0f716e81f1f35b3a
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-09-20 04:52:50 -0400

    Create ExpenseResult

---
+++ ExpenseTrackerAPI/DTOs/ExpenseResult.cs
@@ -0,0 +1,24 @@
+﻿namespace ExpenseTrackerAPI.DTOs;
+
+public class ExpenseResult
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string Category { get; set; }
+
+    public static ExpenseResult FromExpense(Expense expense)
+    {
+        var result = new ExpenseResult
+        {
+            Id = expense.ExpenseId,
+            Description = expense.Description,
+            Amount = expense.Amount,
+            Date = expense.Date,
+            Category = expense.Category.CategoryName,
+        };
+
+        return result;
+    }
+}
