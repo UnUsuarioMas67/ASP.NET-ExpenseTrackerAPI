0b700159cde167e99d1447b1ce4da64beb88ad70
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-11-07 02:52:24 -0400

    Add Result class

---
+++ ExpenseTrackerAPI/Result.cs
@@ -0,0 +1,19 @@
+﻿namespace ExpenseTrackerAPI;
+
+public class Result<T>
+{
+    public T? Value { get; }
+    public string? Error { get; }
+    public bool IsSuccess { get; }
+
+    private Result(T? value, string? error, bool success)
+    {
+        Value = value;
+        Error = error;
+        IsSuccess = success;
+    }
+
+    public static Result<T> Success(T value) => new Result<T>(value, null, true);
+
+    public static Result<T> Failure(string error) => new Result<T>(default, error, false);
+}
